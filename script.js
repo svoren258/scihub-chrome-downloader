62082df9cf6426748798148636cdff537a09973f
Ondrej Svoren <ondrej.svoren@flowup.cz>
2020-08-17 10:32:33 +0200

    feat(script): input args processing implemented

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const { Builder, By, Key } = require('selenium-webdriver');
 const { readFile, appendFileSync } = require('fs');
 
-const regex = /[0-9]{8}/g;
+// Global variables
+const REGEXP = /[0-9]{8}/g;
+const DEFAULT_TIMEOUT = 3000;
+const DEFAULT_INPUT_FILE = 'input.txt';
+const DEFAULT_OUTPUT_FILE = 'output.log';
 
-readFile('/home/ondrejsvoren/Downloads/test.txt', 'utf-8', async (err, data) => {
+const args = processArgs();
+if (args === undefined) {
+  return;
+}
+
+readFile(args['-i'] || DEFAULT_INPUT_FILE, 'utf-8', async (err, data) => {
   if (err) throw err;
-  const pmIDs = data.match(regex);
+  const pmIDs = data.match(REGEXP);
 
   for (const [index, pmID] of pmIDs.entries()) {
     try {
       await download(pmID);
       console.log(`File with PMID: ${pmID} downloaded successfully - ${index + 1}/${pmIDs.length}.`);
       appendFileSync(
-        '/home/ondrejsvoren/Downloads/output.log',
-        `File with PMID: ${pmID} downloaded successfully - ${index + 1}/${pmIDs.length}.`);
+        args['-o'] || DEFAULT_OUTPUT_FILE,
+        `File with PMID: ${pmID} downloaded successfully - ${index + 1}/${pmIDs.length}.\n`);
     } catch (e) {
       console.error(`ERROR: Unable to download file with PMID: ${pmID}.`);
     }
@@ -27,13 +36,56 @@ async function download(input) {
       await driver.get('https://sci-hub.tw/');
       await driver.findElement(By.name('request')).sendKeys(input, Key.ENTER);
       await driver.findElement(By.linkText('⇣ save')).click();
-      await driver.sleep(3000);
+      await driver.sleep(args['-t'] || DEFAULT_TIMEOUT);
       await driver.quit();
       resolve();
     } catch (e) {
-      appendFileSync('/home/ondrejsvoren/Downloads/output.log',`ERROR: Unable to find article with PMID: ${input}.\n`);
+      appendFileSync(args['-o'] || DEFAULT_OUTPUT_FILE,`ERROR: Unable to find article with PMID: ${input}.\n`);
       await driver.quit();
       reject();
     }
   });
 }
+
+// returns input arguments map
+function processArgs() {
+  const args = process.argv.slice(2);
+
+  if (args.length === 0) {
+    console.log('Usage: $ node script.js -i [inputFilePath] -o [outputFilePath] -t [sleepTimeout]');
+    return;
+  }
+
+  const inputArgs = args.reduce((acc, arg, i) => {
+    switch(arg) {
+      case '-i':
+        const inputFilePath = args[i + 1];
+        return {
+          ...acc,
+          ['-i']: inputFilePath
+        };
+      case '-o':
+        const outputFilePath = args[i + 1];
+        return {
+          ...acc,
+          ['-o']: outputFilePath
+        };
+      case '-t':
+        const sleepTimeout = args[i + 1];
+        return {
+          ...acc,
+          ['-t']: sleepTimeout
+        };
+      case '-h':
+        console.log('Usage: $ node script.js -i [inputFilePath] -o [outputFilePath] -t [sleepTimeout]');
+        return;
+      default:
+        return acc;
+    }
+  }, {});
+
+  if (inputArgs !== undefined) {
+    return inputArgs;
+  }
+}
+
